8a1732cf5488d8ee0278f34577e72c15591e3591
Mreza1008 <2006mreza@gmail.com>
2026-02-13 09:18:42 +0330

    add view for reservations

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'reservations'
 
 urlpatterns = [
-    path('reservation_list/', views.reservation_dashboard, name='reservation_lists'),
+    path('reservation_list/', views.reservation_lists, name='reservation_lists'),
     path('reservation_list/pending/', views.pending_list, name='pending_list'),
     path('reservation_list/confirmed/', views.confirmed_list, name='confirmed_list'),
     path('reservation_list/cancelled/', views.cancelled_list, name='cancelled_list'),
--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Reservation
+from .choices import Status 
+
+@login_required
+def reservation_lists(request):
+    return render(request, 'reservations/.html')
+
+@login_required
+def pending_list(request):
+    res = Reservation.objects.filter(user=request.user, status=Status.PENDING)
+    return render(request, 'reservations/status_list.html', {'reservations': res, 'title': 'pending'})
+
+@login_required
+def confirmed_list(request):
+    res = Reservation.objects.filter(user=request.user, status=Status.CONFIRMED)
+    return render(request, 'reservations/status_list.html', {'reservations': res, 'title': 'confirmed'})
+
+@login_required
+def cancelled_list(request):
+    res = Reservation.objects.filter(user=request.user, status=Status.CANCELLED)
+    return render(request, 'reservations/status_list.html', {'reservations': res, 'title': 'cancelled'})
+
+@login_required
+def completed_list(request):
+
+    res = Reservation.objects.filter(user=request.user, status=Status.COMPLETED)
+    return render(request, 'reservations/status_list.html', {'reservations': res, 'title': 'completed'})
