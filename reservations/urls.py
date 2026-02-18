efa4f9063d309979e6b78580f8f905d8222dc4b1
Mreza1008 <2006mreza@gmail.com>
2026-02-18 10:36:59 +0330

    hatml_problem

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'reservations'
 
 urlpatterns = [
-    path('reservation_list/', views.reservation_lists, name='reservation_lists'),
-    path('reservation_list/pending/', views.pending_list, name='pending_list'),
-    path('reservation_list/confirmed/', views.confirmed_list, name='confirmed_list'),
-    path('reservation_list/cancelled/', views.cancelled_list, name='cancelled_list'),
-    path('reservation_list/completed/', views.completed_list, name='completed_list'),
+    path('reservation/', views.reservation_lists, name='reservation_lists'),
+    path('reservation/pending/', views.pending_list, name='pending_list'),
+    path('reservation/confirmed/', views.confirmed_list, name='confirmed_list'),
+    path('reservation/cancelled/', views.cancelled_list, name='cancelled_list'),
+    path('reservation/completed/', views.completed_list, name='completed_list'),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -9,25 +9,25 @@ from .choices import Status
 
 @login_required
 def reservation_lists(request):
-    return render(request, 'reservations/reservation_lists.html')
+    return render(request, 'reservation_list.html')
 
 @login_required
 def pending_list(request):
     res = Reservation.objects.filter(user=request.user, status=Status.PENDING)
-    return render(request, 'reservations/status_list.html', {'reservations': res, 'title': 'pending'})
+    return render(request, 'status_list.html', {'reservations': res, 'title': 'pending'})
 
 @login_required
 def confirmed_list(request):
     res = Reservation.objects.filter(user=request.user, status=Status.CONFIRMED)
-    return render(request, 'reservations/status_list.html', {'reservations': res, 'title': 'confirmed'})
+    return render(request, 'status_list.html', {'reservations': res, 'title': 'confirmed'})
 
 @login_required
 def cancelled_list(request):
     res = Reservation.objects.filter(user=request.user, status=Status.CANCELLED)
-    return render(request, 'reservations/status_list.html', {'reservations': res, 'title': 'cancelled'})
+    return render(request, 'status_list.html', {'reservations': res, 'title': 'cancelled'})
 
 @login_required
 def completed_list(request):
 
-    res = Reservation.objects.filter(user=request.user, status=Status.COMPLETED)
-    return render(request, 'reservations/status_list.html', {'reservations': res, 'title': 'completed'})
+    res = Reservation.objects.filter(user=request.user, status=Status.COMPELETED)
+    return render(request, 'status_list.html', {'reservations': res, 'title': 'completed'})
